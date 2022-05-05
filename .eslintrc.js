2878319b53a5c53930e05d9dea29c92e35188a4c
unknown <matheuspedroso404@gmail.com>
2022-05-05 14:05:45 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "google",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+  ],
+
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    quotes: [2, "double", { avoidEscape: true }],
+    "quote-props": [2, "as-needed"],
+    "object-curly-spacing": [2, "always"],
+    "react/react-in-jsx-scope": "off",
+    "require-jsdoc": 0,
+    "spaced-comment": [2, "always"],
+  },
+  settings: {
+    react: {
+      version: "latest",
+    },
+  },
+};
